81b9dbda5c7429fdd770679f21d76a042096ad78
Lucas Amado <syst.lucasamado@gmail.com>
2021-10-24 20:08:15 -0300

    Criando button para loggout da aplicação

--- src/main/java/br/com/amado/rhalpha/WebSecurityConfig.java
+++ src/main/java/br/com/amado/rhalpha/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 			)
 			.logout(logout -> {
 				logout.logoutUrl("/logout")
-				.logoutSuccessUrl("/home");
+				.logoutSuccessUrl("/login");
 			})
 			.csrf().disable();
 	}
