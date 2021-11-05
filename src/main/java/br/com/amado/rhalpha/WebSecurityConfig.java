468e26d7d7a3d8d63b2cece2a0e91e2cb6befe27
Lucas Amado <syst.lucasamado@gmail.com>
2021-11-05 18:05:48 -0300

    Implementação monitor
    Implementação do spring actuator para subir uma api para monitoramento do servidor e banco. Além de implementação em projeto paralelo com spring boot admin

--- src/main/java/br/com/amado/rhalpha/WebSecurityConfig.java
+++ src/main/java/br/com/amado/rhalpha/WebSecurityConfig.java
@@ -19,7 +19,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	DataSource dataSource;
-	
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests().anyRequest().permitAll()
+//				.and().csrf().disable();
+//	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
